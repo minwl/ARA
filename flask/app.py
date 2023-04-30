9d3c7035a4565a57460ca89839ad3eea506f72f5
minwl <jhmin325@gmail.com>
2023-04-30 16:38:59 +0900

    initial upload

---
+++ flask/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, render_template, request, send_from_directory, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+   query = request.form.get('query')
+
+   if query:
+       print('Request for hello page received with query=%s' % query)
+       return render_template('result.html', query = query)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+   app.run(debug=True)
