701eebdf89978fb16a49a40ef4438779c27f36e2
minwl <jhmin325@gmail.com>
2023-05-06 00:47:35 +0900

    update javascript

--- flask/app.py
+++ flask/app.py
@@ -7,16 +7,23 @@ def index():
    print('Request for index page received')
    return render_template('index.html')
 
-@app.route('/result', methods=['POST'])
-def result():
-   query = request.form.get('query')
+# @app.route('/api')
+# def myapi():
+#   args = request.args
+#   query = args.get('query')
+#   if query:  return dict(success=1, result=query)
+#   else : return redirect(url_for('index'))
 
-   if query:
-       print('Request for hello page received with query=%s' % query)
-       return render_template('result.html', query = query)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+# @app.route('/result', methods=['POST'])
+# def result():
+#    query = request.form.get('query')
+
+#    if query:
+#        print('Request for hello page received with query=%s' % query)
+#        return render_template('result.html', query = query)
+#    else:
+#        print('Request for hello page received with no name or blank name -- redirecting')
+#        return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
